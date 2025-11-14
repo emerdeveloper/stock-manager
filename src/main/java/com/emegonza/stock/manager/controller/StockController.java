17f07b8d47fbb994b65c5fb5e7b9932f55919303
Emerson Javid Gonzalez <emersonjavid97@gmail.com>
2025-11-14 08:02:44 -0500

    create packages structure and basic controller class

---
+++ src/main/java/com/emegonza/stock/manager/controller/StockController.java
@@ -0,0 +1,11 @@
+package com.emegonza.stock.manager.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/stock", produces = MediaType.APPLICATION_JSON_VALUE)
+public class StockController {
+
+}
