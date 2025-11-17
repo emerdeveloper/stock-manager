a0f55a771259c2a0f70982e766b98bb6f9a5f965
Emerson Javid Gonzalez <emersonjavid97@gmail.com>
2025-11-17 13:52:01 -0500

    add POST method to expose endpoint to save product

--- src/main/java/com/emegonza/stock/manager/controller/StockController.java
+++ src/main/java/com/emegonza/stock/manager/controller/StockController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,12 @@ public class StockController {
         return service.getProduct(id)
                 .map(product -> ResponseEntity.status(HttpStatus.OK).body(product))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> sveProduct(@RequestBody ProductDto product) {
+        return service.saveProduct(product)
+                .map(productSaved -> ResponseEntity.status(HttpStatus.CREATED).body(product))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 }
