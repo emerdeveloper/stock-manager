a08e76f914148949b333c90456414d11d0d164f7
Emerson Javid Gonzalez <emersonjavid97@gmail.com>
2025-11-17 13:51:26 -0500

    add method to save product

--- src/main/java/com/emegonza/stock/manager/service/ProductService.java
+++ src/main/java/com/emegonza/stock/manager/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.emegonza.stock.manager.service;
 
 import com.emegonza.stock.manager.model.ProductDto;
+import com.emegonza.stock.manager.model.ProductEntity;
 import com.emegonza.stock.manager.repository.IProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,18 @@ public class ProductService {
     public Optional<ProductDto> getProduct(int id) {
         return repository.findById(id)
                 .map(Mapper::productEntityToDto);
+    }
+
+    public Optional<ProductDto> saveProduct(ProductDto product) {
+        return Optional.ofNullable(product.id())
+                .filter(id -> repository.findById(id).isEmpty())
+                .map(id -> {
+                    ProductEntity entity = repository.save(Mapper.productDtoToEntity(product));
+                    return Mapper.productEntityToDto(entity);
+                })
+                .or(() -> {
+                    ProductEntity entity = repository.save(Mapper.productDtoToEntity(product));
+                    return Optional.of(Mapper.productEntityToDto(entity));
+                });
     }
 }
