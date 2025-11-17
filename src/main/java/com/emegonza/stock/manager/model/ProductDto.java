eb3a359dfcd47ffbde3b91f06b31b062e473475c
Emerson Javid Gonzalez <emersonjavid97@gmail.com>
2025-11-17 13:49:50 -0500

    change dataType of dates and add date format

--- src/main/java/com/emegonza/stock/manager/model/ProductDto.java
+++ src/main/java/com/emegonza/stock/manager/model/ProductDto.java
@@ -1,16 +1,22 @@
 package com.emegonza.stock.manager.model;
 
 import com.emegonza.stock.manager.enums.ProductStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.Date;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(value = JsonInclude.Include.NON_NULL, content = JsonInclude.Include.NON_EMPTY)
-public record ProductDto (String name,
+public record ProductDto (Integer id,
+                          String name,
                           String enteredByUser,
-                          String enteredDate,
+                          @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-M-yyyy hh:mm:ss a")
+                          Date enteredDate,
                           double buyingPrince,
                           double sellingPrice,
-                          String lastModifiedDate,
+                          @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-M-yyyy hh:mm:ss a")
+                          Date lastModifiedDate,
                           String lastModifiedByUser,
                           ProductStatus status) { }
